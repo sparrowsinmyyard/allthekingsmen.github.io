1c99b85865db7b9734520bba49417bb2eae45c10
sparrowsinmyyard <sparrowsinmyyard@gmail.com>
2025-12-23 16:19:40 +0530

    Update timer.js

--- timer.js
+++ timer.js
@@ -2,11 +2,21 @@ function calculateReadingTime() {
   const articleText = document.querySelector('article').innerText;
   const wordCount = articleText.split(/\s+/).length;
   const readingTimeMinutes = Math.ceil(wordCount / 200);
-  const readingTimeSpan = document.getElementById('readingTime');
   
-  if (readingTimeSpan) {
-    readingTimeSpan.textContent = `${readingTimeMinutes} min read`;
-  }
+  // CREATE the reading time element
+  const readingTimeDiv = document.createElement('div');
+  readingTimeDiv.className = 'reading-time';
+  readingTimeDiv.innerHTML = `
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+      <circle cx="12" cy="12" r="10"></circle>
+      <polyline points="12 6 12 12 16 14"></polyline>
+    </svg>
+    <span>${readingTimeMinutes} min read</span>
+  `;
+  
+  // INSERT it after the h1
+  const h1 = document.querySelector('article h1');
+  h1.insertAdjacentElement('afterend', readingTimeDiv);
 }
 
 calculateReadingTime();
