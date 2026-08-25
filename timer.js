7da1e3f6d6bef5363f8c16fc575c4872853db7d1
aryanpatel <sparrowsinmyyard@gmail.com>
2026-08-26 00:04:44 +0530

    Enhance reading time output with word count
    Updated reading time display to include word count.

--- timer.js
+++ timer.js
@@ -9,14 +9,14 @@ function calculateReadingTime() {
   const wordCount = articleText.split(/\s+/).length;
   const readingTimeMinutes = Math.ceil(wordCount / 200);
   
-  const readingTimeDiv = document.createElement('div');
+const readingTimeDiv = document.createElement('div');
   readingTimeDiv.className = 'reading-time';
   readingTimeDiv.innerHTML = `
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
       <circle cx="12" cy="12" r="10"></circle>
       <polyline points="12 6 12 12 16 14"></polyline>
     </svg>
-    <span>${readingTimeMinutes} min read</span>
+    <span>${readingTimeMinutes} min read • ${wordCount} words</span>
   `;
   
   const h1 = document.querySelector('article h1');
